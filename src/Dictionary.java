df245002eb097f5153818ee8530e3e22205033bb
Stephen Hildebrand <sfhildeb@gmail.com>
2017-06-29 18:38:45 -0400

    Improved organization of code and documentation.

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -5,54 +5,62 @@ import java.util.Random;
 /**
  * @author Stephen Hildebrand
  * @version 4/22/2016
- *          <p>
- *          Project: Project4
- * This class implements a Dictionary of Word objects as a hash table that maps
- * keys to values.
+ *          This class implements a Dictionary of Word objects as a hash table that maps
+ *          keys to values.
  */
 public class Dictionary {
-    private int n = 0;                  /** number of dictionary entries in the table */
-    private int capacity;               /** the number of buckets in the hash table */
-    private int prime;                  /** prime factor */
-    private long scale, shift;          /** shift and scaling factors */
-    private LinkedList<Word>[] table;   /** Array of LinkedList word buckets */
-    private int probeCount = 0;         /** total number of probes made during checking */
-    private int lookupCount = 0;        /** count of lookup operations */
+    /** The number of dictionary entries in the table. */
+    private int n = 0;
+    /** The number of buckets in the hash table. */
+    private int capacity;
+    /** Prime factor. */
+    private int prime;
+    /** Shift and scaling factors. */
+    private long scale, shift;
+    /** Array of LinkedList word buckets. */
+    private LinkedList<Word>[] table;
+    /** Total number of probes made during checking. */
+    private int probeCount = 0;
+    /** Count of lookup operations. */
+    private int lookupCount = 0;
 
 
-
-    @SuppressWarnings({"unchecked"})
     /** Creates a hash table with the given capacity and prime factor (31957). */
+    @SuppressWarnings({"unchecked"})
     public Dictionary(int capacity, int prime) {
         this.capacity = capacity;
         this.prime = prime;
         Random rand = new Random();
-        scale = rand.nextInt(prime-1) + 1;
+        scale = rand.nextInt(prime - 1) + 1;
         shift = rand.nextInt(prime);
         table = (LinkedList<Word>[]) new LinkedList[capacity];
     }
 
     /** Creates a hash table with given capacity and prime factor 109345121. */
-    public Dictionary(int capacity) { this(capacity, 109345121); }  // default prime
+    public Dictionary(int capacity) {
+        this(capacity, 109345121);
+    }  // Default prime.
 
     /**
      * Implementation of a 5-bit cyclic-shift to compute the hash key for a
      * string word.
+     *
      * @param word the word to look up
-     * @return  the calculated key
+     * @return the calculated key
      */
-   public int hashCode(String word) {
+    public int hashCode(String word) {
         int k = 0;
         for (int i = 0; i < word.length(); i++) {
-            k = (k << 5) | (k >>> 27);      // 5-bit cyclic shift of the running sum
-            k += (int) word.charAt(i);      // add in the next character
+            k = (k << 5) | (k >>> 27);      // 5-bit cyclic shift of the running sum.
+            k += (int) word.charAt(i);      // Add in the next character.
         }
         return k;
     }
 
     /**
      * Returns the value associated with the specified key, or null if no such entry exists.
-     * @param key  the key whose associated value is to be returned
+     *
+     * @param key the key whose associated value is to be returned
      * @return the associated value, or null if no such entry exists
      */
     public String get(int key) {
@@ -63,18 +71,22 @@ public class Dictionary {
     /**
      * Removes the entry with the specified key, if present, and returns
      * its associated value. Otherwise does nothing and returns null.
-     * @param key  the key whose entry is to be removed from the map
+     *
+     * @param key the key whose entry is to be removed from the map
      * @return the previous value associated with the removed key, or null if no such entry exists
      */
-    public String remove(int key) { return bucketRemove(hashValue(key), key); }
+    public String remove(int key) {
+        return bucketRemove(hashValue(key), key);
+    }
 
     /**
      * Associates the given value with the given key. If an entry with
      * the key was already in the map, this replaces the previous value
      * with the new one and returns the old value. Otherwise, a new
      * entry is added and null is returned.
-     * @param key    key with which the specified value is to be associated
-     * @param value  value to be associated with the specified key
+     *
+     * @param key   key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
      * @return the previous value associated with the key (or null, if no such entry)
      */
     public String put(int key, String value) {
@@ -82,24 +94,29 @@ public class Dictionary {
     }
 
     // private utilities
+
     /** Hash function applying MAD method to default hash code. */
     private int hashValue(int key) {
-        return (int) ((Math.abs(key*scale + shift) % prime) % capacity);
+        return (int) ((Math.abs(key * scale + shift) % prime) % capacity);
     }
 
     /**
      * Returns the number of words currently in the dictionary.
-     * @return  the number of dictionary entries
+     *
+     * @return the number of dictionary entries
      */
-    public int size() { return n; }
+    public int size() {
+        return n;
+    }
 
 
     /**
      * Returns value associated with key k in bucket with hash value h.
      * If no such entry exists, returns null.
-     * @param h  the hash value of the relevant bucket
-     * @param k  the key of interest
-     * @return   associate value (or null, if no such entry)
+     *
+     * @param h the hash value of the relevant bucket
+     * @param k the key of interest
+     * @return associate value (or null, if no such entry)
      */
     private String bucketGet(int h, int k) {
         LinkedList<Word> bucket = table[h];
@@ -116,10 +133,11 @@ public class Dictionary {
     /**
      * Associates key k with value v in bucket with hash value h, returning
      * the previously associated value, if any.
-     * @param h  the hash value of the relevant bucket
-     * @param k  the key of interest
-     * @param v  the value to be associated
-     * @return   previous value associated with k (or null, if no such entry)
+     *
+     * @param h the hash value of the relevant bucket
+     * @param k the key of interest
+     * @param v the value to be associated
+     * @return previous value associated with k (or null, if no such entry)
      */
     private String bucketPut(int h, int k, String v) {
         LinkedList<Word> bucket = table[h];
@@ -141,9 +159,10 @@ public class Dictionary {
     /**
      * Removes entry having key k from bucket with hash value h, returning
      * the previously associated value, if found.
-     * @param h  the hash value of the relevant bucket
-     * @param k  the key of interest
-     * @return   previous value associated with k (or null, if no such entry)
+     *
+     * @param h the hash value of the relevant bucket
+     * @param k the key of interest
+     * @return previous value associated with k (or null, if no such entry)
      */
     private String bucketRemove(int h, int k) {
         LinkedList<Word> bucket = table[h];
@@ -177,6 +196,7 @@ public class Dictionary {
     /**
      * Returns the total number of times a text word is compared to a word
      * in the table.
+     *
      * @return the number of times a text word is compared to a table word
      */
     public int getProbeCount() {
@@ -185,6 +205,7 @@ public class Dictionary {
 
     /**
      * Returns the total number of times a lookup operation is performed.
+     *
      * @return the number of times a text word is compared to a table word
      */
     public int getLookupCount() {
@@ -195,8 +216,9 @@ public class Dictionary {
      * A private nested class for a Word implemented as a key-value pair,
      * with integer keys and String values.
      */
-    private static class Word implements Comparable<Word>{
-        private int key;            /** the key to be stored */
+    private static class Word implements Comparable<Word> {
+        private int key;
+        /** the key to be stored */
         private String value;       /** the hashed value associated with key */
 
         /** Constructor for a new Word. */
@@ -207,31 +229,47 @@ public class Dictionary {
 
         /**
          * Returns the key (word).
-         * @return  the key for this Word
+         *
+         * @return the key for this Word
          */
-        public int getKey() { return key; }
+        public int getKey() {
+            return key;
+        }
+
         /**
          * Returns the associated word as a string.
-         * @return  the associated word
+         *
+         * @return the associated word
          */
-        public String getValue() { return value; }
+        public String getValue() {
+            return value;
+        }
+
         /**
          * Sets the key (word).
-         * @param key   the key to set for this Word
+         *
+         * @param key the key to set for this Word
          */
-        public void setKey(int key) { this.key = key; }
+        public void setKey(int key) {
+            this.key = key;
+        }
+
         /**
          * Sets the associated value, then returns the previously associated value.
+         *
          * @param v the string for this Word
-         * @return  the previously associated value (may be null)
+         * @return the previously associated value (may be null)
          */
         public String setValue(String v) {
             String old = this.value;
             this.value = v;
             return old;
         }
+
         /** Returns a string representation of this Word. */
-        public String toString() { return "<" + key + ", " + value + ">"; }
+        public String toString() {
+            return "<" + key + ", " + value + ">";
+        }
 
         @Override
         public int compareTo(Word o) {
--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -6,29 +6,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * @author  Stephen Hildebrand
- * @version 4/22/2016
- *          <p>
- *          Project: Project4
+ * @author Stephen Hildebrand
+ * @version 2017/06/29
  */
 public class SpellCheck {
-    private static Dictionary dict;                 /** dictionary hash table of words */
-    private static final int DICT_SIZE = 25144;     /** number of words in the default dictionary */
-    private static final int EXIT_FAILURE = 1;      /** error exit status code of 1 */
+    /** Dictionary hash table of words. */
+    private static Dictionary dict;
+    /** Number of words in the default dictionary. */
+    private static final int DICT_SIZE = 25144;
+    /** Error exit status code of 1. */
+    private static final int EXIT_FAILURE = 1;
 
     /**
      * The main method. Begins by reading a list of edges from a text file.
      * Prompts for name of dictionary file containing the word list, the name
      * of the text file to be checked and the name of the output file.
-     * @param args  The command-line arguments
+     *
+     * @param args The command-line arguments
      */
     public static void main(String[] args) {
-        String txtFileName;                     /** name of the input text file */
-        int dictWordCount = 0;                  /** count of words in the dictionary */
-        int textWordCount = 0;                  /** count of words in the text */
-        int misspelledCount = 0;                /** count of misspelled words */
-        String outFileName = null;              /** name of the output file */
-        PrintStream outfile = null;             /** stream to print to output file */
+        String txtFileName;                     /* Name of the input text file. */
+        int dictWordCount = 0;                  /* Count of words in the dictionary. */
+        int textWordCount = 0;                  /* Count of words in the text. */
+        int misspelledCount = 0;                /* Count of misspelled words. */
+        String outFileName = null;              /* Name of the output file. */
+        PrintStream outfile = null;             /* Stream to print to output file. */
 
         if (args == null || args.length != 1) {
             System.out.printf("Error reading input file...\nUsage: SpellCheck <input-file.txt>\n");
@@ -39,17 +41,17 @@ public class SpellCheck {
         //      Dictionary File Processing
         //-------------------------------------
         dict = new Dictionary(DICT_SIZE);
-        // read/process dictionary file and fill hash table
+        // Read/process dictionary file and fill hash table.
         try {
             Scanner ds = new Scanner(new File("dict.txt"));
             while (ds.hasNextLine()) {
                 Scanner ls = new Scanner(ds.nextLine());
                 String word = ls.next();
-                dict.put(dict.hashCode(word), word);    // add word to dictionary
+                dict.put(dict.hashCode(word), word);    // Add word to dictionary.
 
-                dictWordCount++;        // increment total dictionary word count
+                dictWordCount++;                        // Increment total dictionary word count.
             }
-        } catch (FileNotFoundException e) { // Catch file IO error and quit
+        } catch (FileNotFoundException e) {             // Catch file IO error and quit.
             System.out.println("Error. Dictionary file <dict.txt> could not be found.");
             System.exit(EXIT_FAILURE);
         } // --------- End Dictionary File ----------
@@ -58,7 +60,7 @@ public class SpellCheck {
         //-------------------------------------
         //         Text File Processing
         //-------------------------------------
-        // read and process input text file & check spelling of each word
+        // Read and process input text file & check spelling of each word.
         txtFileName = args[0];
         if (txtFileName != null && txtFileName.length() > 0) {
             try {
@@ -66,21 +68,21 @@ public class SpellCheck {
                 outFileName = txtFileName.replace(".txt", "-out.txt");
                 outfile = new PrintStream(new File(outFileName));
                 outfile.printf("Possible misspelled words:\n");
-                // for each line of the text file
+                // For each line of the text file.
                 while (ts.hasNextLine()) {
-                    String line = ts.nextLine();                    // next line of text
-                    Pattern pat = Pattern.compile("[\\w']+");       // regex pattern
-                    Matcher match = pat.matcher(line);              // matcher for pattern
+                    String line = ts.nextLine();                    // Next line of text.
+                    Pattern pat = Pattern.compile("[\\w']+");       // Regex pattern.
+                    Matcher match = pat.matcher(line);              // Matcher for pattern.
                     while (match.find()) {
                         String word = line.substring(match.start(), match.end());
                         textWordCount++;
                         if (!checkSpelling(word)) {
-                            outfile.printf(word + "\n");            // print to output file
+                            outfile.printf(word + "\n");            // Print to output file.
                             misspelledCount++;
                         }
                     }
                 }
-            } catch (FileNotFoundException e) { // Catch file IO error and quit
+            } catch (FileNotFoundException e) {                     // Catch file IO error and quit.
                 System.out.printf("Unable to read %s. Usage: SpellCheck <input-file.txt>\n", txtFileName);
                 System.exit(EXIT_FAILURE);
             }
@@ -96,16 +98,16 @@ public class SpellCheck {
         System.out.printf("Printing results to %s.\n", outFileName);
         int probeCount = dict.getProbeCount();
         int lookupCount = dict.getLookupCount();
-        outfile.printf("\nDictionary word count: %d\n", dictWordCount);  // count of words in dictionary
-        outfile.printf("Text word count: %d\n", textWordCount);          // count of words in the text
-        outfile.printf("Misspelled word count: %d\n", misspelledCount);  // count of misspelled words
-        outfile.printf("Total probe count: %d\n", probeCount);           // count of total probes
+        outfile.printf("\nDictionary word count: %d\n", dictWordCount);  // Count of words in dictionary.
+        outfile.printf("Text word count: %d\n", textWordCount);          // Count of words in the text.
+        outfile.printf("Misspelled word count: %d\n", misspelledCount);  // Count of misspelled words.
+        outfile.printf("Total probe count: %d\n", probeCount);           // Count of total probes.
 
-        // average number of probes per word
+        // Average number of probes per word.
         double probesPerWord = probeCount / (double) textWordCount;
         outfile.printf("Average number of probes per word: %.2f\n", probesPerWord);
 
-        // average number of probes per lookup operation
+        // Average number of probes per lookup operation.
         double probesPerLookup = probeCount / (double) lookupCount;
         outfile.printf("Average number of probes per lookup: %.2f\n", probesPerLookup);
         // ---------- End Output File ------------
@@ -119,57 +121,58 @@ public class SpellCheck {
      * strip common suffixes, and the search is then reattempted. Regards the
      * apostrophe as a letter (e.g. "don't"). The number of lookups is tracked in the
      * Dictionary class.
-     * @param word  the word to check
+     *
+     * @param word the word to check
      * @return true if the word is found in the dictionary, false if not
      */
     private static boolean checkSpelling(String word) {
-        boolean found = false;              /** whether a match has been found */
-//        String retry;                       /** altered word to retry */
+        boolean found = false;              /* Whether or not a match has been found. */
+//        String retry;                       /* Altered word to retry. */
 
-        // first lookup for dictionary match
+        // First lookup for dictionary match.
         int key = dict.hashCode(word);
         String ans = dict.get(key);
         if (ans != null)
-            found = ans.equals(word);       // additional check to ensure correct word returned
+            found = ans.equals(word);       // Additional check to ensure correct word returned.
 
         // ----------- Word Alterations ------------
-        // if upper case first letter -> downshift to lower case
+        // If upper case first letter -> downshift to lower case.
         if (!found && Character.isUpperCase(word.charAt(0))) {
             word = Character.toLowerCase(word.charAt(0)) + word.substring(1);
             key = dict.hashCode(word);
             found = word.equals(dict.get(key));
         }
-        // suffix = "'s" drop
+        // Suffix = "'s" drop.
         if (!found && word.endsWith("'s")) {
             word = word.substring(0, word.length() - 2);
             key = dict.hashCode(word);
             found = word.equals(dict.get(key));
         }
-        // suffix = "s" drop
+        // Suffix = "s" drop.
         if (!found && word.endsWith("s")) {
             word = word.substring(0, word.length() - 1);
             key = dict.hashCode(word);
             found = word.equals(dict.get(key));
-            // suffix = "es"
+            // Suffix = "es".
             if (!found && word.endsWith("es")) {
                 word = word.substring(0, word.length() - 2);
                 key = dict.hashCode(word);
                 found = word.equals(dict.get(key));
             }
         }
-        // suffix = "ed" drop
+        // Suffix = "ed" drop.
         if (!found && word.endsWith("ed")) {
             word = word.substring(0, word.length() - 2);
             key = dict.hashCode(word);
             found = word.equals(dict.get(key));
-            // suffix "d" drop
+            // Suffix "d" drop.
             if (!found && word.endsWith("d")) {
                 word = word.substring(0, word.length() - 1);
                 key = dict.hashCode(word);
                 found = word.equals(dict.get(key));
             }
         }
-        // suffix = "er" drop
+        // Suffix = "er" drop.
         if (!found && word.endsWith("er")) {
             word = word.substring(0, word.length() - 2);
             key = dict.hashCode(word);
@@ -181,25 +184,25 @@ public class SpellCheck {
                 found = word.equals(dict.get(key));
             }
         }
-        // suffix = "ing" drop
+        // Suffix = "ing" drop.
         if (!found && word.endsWith("ing")) {
             word = word.substring(0, word.length() - 3);
             key = dict.hashCode(word);
             found = word.equals(dict.get(key));
-            // replace "ing" with suffix = "e"
+            // Replace "ing" with suffix = "e".
             if (!found) {
                 word = word + "e";
                 key = dict.hashCode(word);
                 found = word.equals(dict.get(key));
             }
         }
-        // suffix = "ly" drop
+        // Suffix = "ly" drop.
         if (!found && word.endsWith("ly")) {
             word = word.substring(0, word.length() - 2);
             key = dict.hashCode(word);
             found = word.equals(dict.get(key));
         }
 
-        return found;       // true if any one of the above was successful
+        return found;       // True if any one of the above was successful.
     }
 }
